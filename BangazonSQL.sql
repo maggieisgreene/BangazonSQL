e52886a1737a13c089a33a1bb0adb027786cb4cb
Maggie Greene <maggie.greene96@gmail.com>
2020-04-07 13:22:35 -0500

    Initial commit-- project setup

---
+++ BangazonSQL.sql
@@ -0,0 +1,3 @@
+﻿--List each employee first name, last name and supervisor status along with their department name.
+--Order by department name, then by employee last name, and finally by employee first name.
+
